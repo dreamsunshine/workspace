7153d779c0553f9992845e1a394ec4137383d704
dreamsunshine <498371067@qq.com>
2016-01-12 09:29:35 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -1,29 +1,28 @@
-Zepto(function($){
-    $('.setuptip').css('opacity','1').find('.setuptip_sub2,.setuptip_sub3').addClass('slide-enter');
-	$('.setuptip').tap(function(e){
+Zepto(function($) {
+	$('.setuptip').css('opacity', '1').find('.setuptip_sub2,.setuptip_sub3').addClass('slide-enter');
+	$('.setuptip').tap(function(e) {
 		e.preventDefault();
 		$(this).toggle();
-	})
-	$('.setuptip').swipe(function(e){
+	});
+	$('.setuptip').swipe(function(e) {
 		e.preventDefault();
-	})
-	var pic_w=$('.infopic').width();
+	});
+	var pic_w = $('.infopic').width();
 	$('.infopic').height(pic_w);
-//	$('.infopic img').attr('src','').css('background-image','url(./dist/img/qrcodebtn.png)')
-	function picmiddle(obj,url,w){
-		var img=new Image();
-		img.src=url;
-		if(img.complete){
-			
-		}else{
-			img.onload=function(){
-				var pw=img.width,ph=img.height;
-				if(pw<w-4){
-					obj.find('img').css('margin-top',(w-4-ph)/2+'px').attr('src',url)
+	//	$('.infopic img').attr('src','').css('background-image','url(./dist/img/qrcodebtn.png)')
+	function picmiddle(obj, url, w) {
+		var img = new Image();
+		img.src = url;
+		if (img.complete) {
+
+		} else {
+			img.onload = function() {
+				var pw = img.width,
+					ph = img.height;
+				if (pw < w - 4) {
+					obj.find('img').css('margin-top', (w - 4 - ph) / 2 + 'px').attr('src', url);
 				}
-			
+			};
 		}
-		}
-		
 	}
-})
+});
